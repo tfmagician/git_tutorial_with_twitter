398921d97db716c792396e1b0ddedeae63818877
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2011-12-15 20:40:26 +0900

    change mention to favorite

--- git_tutorial_with_twitter/application.py
+++ git_tutorial_with_twitter/application.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 def show_timeline(screenname):
     return render_template('timeline.html',tweets=datasource.timeline(screenname))#datasource.timeline(screenname)
 
-@app.route('/<screenname>/mention')
-def show_mention(screenname):
-    return render_template('timeline.html',tweets=datasource.mention(screenname))#datasource.mention(screenname)
+@app.route('/<screenname>/favorites')
+def show_favorites(screenname):
+    return render_template('timeline.html',tweets=datasource.favorites(screenname))#datasource.mention(screenname)
 
 if __name__ == '__main__':
     if os.environ.get('FLASK_ENV') == 'DEVELOP':
         show_timeline('muro') 
-        show_mention('hoshi')
+        show_favorites('hoshi')
     else:
         app.run(debug=True,host='0.0.0.0',port=8080)
--- git_tutorial_with_twitter/datasource.py
+++ git_tutorial_with_twitter/datasource.py
@@ -1,5 +1,5 @@
 def timeline(screen_name):
     return screen_name
 
-def mention(screen_name):
+def favorites(screen_name):
     return screen_name
